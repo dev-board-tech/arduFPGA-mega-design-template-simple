dea91faf0f5d3c68855e1035ff41088d32e50868
Iulian Gheorghiu <morgoth@devboard.tech>
2020-05-07 21:02:06 +0300

    Add license header, delete commented code.

--- arduFPGA_simple_template_app/arduFPGA_simple_template_app/main.c
+++ arduFPGA_simple_template_app/arduFPGA_simple_template_app/main.c
@@ -1,9 +1,22 @@
 /*
- * arduFPGA_simple_template_app.c
- *
- * Created: 07.05.2020 11:09:39
- * Author : MorgothCreator
- */ 
+ * Main application for the simplest Mega design.
+ * 
+ * Copyright (C) 2020  Iulian Gheorghiu (morgoth@devboard.tech)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -25,23 +38,19 @@ static void delay_ms(uint32_t time)
 	cli();
 	uint32_t time_to_tick = tim_cnt + time;
 	sei();
-	//uint32_t rtc_cnt_int;
 	while(1) {
 		if(tim_cnt > time_to_tick)
 			return;
 	}
-	/*do
-	{
-		rtc_cnt_int = tim_cnt;
-	} while (time_to_tick > rtc_cnt_int);*/
 }
 
 int main(void)
 {
     // In this example the design use a handwritten RTC as TIMER0 overflow interrupt, so we do not need to initialize the TIMER.
-	// As soon as we enable the interrupts the core will receive them.
+	// If you use arduino, even if arduino IDE initialize TIMER0, will have no effect.
+	// As soon as we enable the interrupts the core will receive interrupts from TIMER0 overflow.
 	DDRB = (LED_B | LED_G | LED_R); // Activate the outputs for LED pins.
-	// We can not set up PULL UP's from software for the inputs, PULL UPs on iCE40UP device are set up from the design.
+	// We can not set up PULL UP's from software for the inputs, PULL UP's on iCE40UP device are set up from the design.
 	sei();
 	PORTB = LED_R;
 	delay_ms(1000);
@@ -50,13 +59,13 @@ int main(void)
 	PORTB = LED_G;
 	delay_ms(1000);
 	PORTB = 0;
-	uint8_t tmp_keys_old = 0;
+	uint8_t old_keys = 0;
     while (1) 
     {
 		 // The advantage of the FPGA is that on the same IO PORT we can use outputs separate from the inputs.
 		 // In this case for keyboard we use inputs 5:0, and for LED's we use outputs 2:0 of the same IO PORT, so, pins 2:0 has dual function.
 		uint8_t tmp_keys = (~PINB) & 0b00011111; // Inverse the inputs from PORTB and filter them, the common for keyboard is logic 0.
-		if(tmp_keys_old != tmp_keys) {
+		if(old_keys != tmp_keys) {
 			switch(tmp_keys) {
 				case 0b00000001: // BTN_OK
 					PORTB = LED_R;
@@ -77,11 +86,12 @@ int main(void)
 					PORTB = 0;
 					break;
 			}
-			tmp_keys_old = tmp_keys;
+			old_keys = tmp_keys;
 		}
     }
 }
 
+// Interrupt service routine.
 ISR(TIMER0_OVF_vect) {
 	tim_cnt++;
 }
